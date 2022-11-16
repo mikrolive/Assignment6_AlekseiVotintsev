10f0e57f712da73b41f187648f8f49e181ea0e81
mikro <mikro@DESKTOP-USKVP4O>
2022-11-16 15:07:32 +0200

    Добавьте файлы проекта.

---
+++ Assignment 6/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Assignment_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Paper = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(66, 112);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 83);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Rock";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Rock_Click);
+            // 
+            // Paper
+            // 
+            this.Paper.Location = new System.Drawing.Point(188, 112);
+            this.Paper.Name = "Paper";
+            this.Paper.Size = new System.Drawing.Size(99, 83);
+            this.Paper.TabIndex = 1;
+            this.Paper.Text = "Paper";
+            this.Paper.UseVisualStyleBackColor = true;
+            this.Paper.Click += new System.EventHandler(this.Paper_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(310, 112);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(99, 83);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Scissors";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Scissors_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 311);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Paper);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Paper;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Assignment 6/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment_6
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        int player = 0;
+        int AI = 0;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Paper_Click(object sender, EventArgs e)
+        {
+            AI = random.Next(1, 4);
+            player = 2;
+
+            if (AI == 1)
+            {
+                MessageBox.Show("Win");
+            }
+            else if (AI == 2)
+            {
+                MessageBox.Show("Draw");
+            }
+            else
+            {
+                MessageBox.Show("Loss");
+            }
+
+        }
+        private void Rock_Click(object sender, EventArgs e)
+        {
+            AI = random.Next(1, 4);
+            player = 1;
+
+            if (AI == 1)
+            {
+                MessageBox.Show("Draw");
+            }
+            else if (AI == 2)
+            {
+                MessageBox.Show("Loss");
+            }
+            else
+            {
+                MessageBox.Show("Win");
+            }
+
+        }
+
+        private void Scissors_Click(object sender, EventArgs e)
+        {
+            AI = random.Next(1, 4);
+            player = 3;
+
+            if (AI == 1)
+            {
+                MessageBox.Show("Loss");
+            }
+            else if (AI == 2)
+            {
+                MessageBox.Show("Win");
+            }
+            else
+            {
+                MessageBox.Show("Draw");
+            }
+        }
+    }
+}
